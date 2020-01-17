aeb3d4a13b2bc589222596fd132e74278608b3c3
Wen Cao -X (wenca - Pactera at Cisco) <wenca@cisco.com>
2020-01-17 23:44:06 +0800

    add CertificateFile.vue

---
+++ frontend/src/views/CertificateFile.vue
@@ -0,0 +1,29 @@
+<template>
+<div id='crt_file'>
+	<el-col :span="12">
+		<h3>Certificate content is:</h3>
+		<el-input type="textarea" v-model="crt_file" rows="24" readonly></el-input>
+		<br><br>
+		<el-button type="primary" icon="el-icon-arrow-left" @click="back">Back</el-button>
+	</el-col>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'CertificateFile',
+	data () {
+		return {
+			'crt_file': ''
+		}
+	},
+	created () {
+		this.crt_file = this.$store.state.crt_file
+	},
+	methods: {
+		back () {
+			this.$router.go(-1)
+		}
+	}
+}
+</script>
